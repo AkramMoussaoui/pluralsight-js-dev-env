443ee6e9b74f193ac434951565e61fe52c43171f
Akram <akrammoussaoui14@gmail.com>
2019-06-26 13:30:19 +0100

    add api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -26,4 +26,12 @@ app.listen(port, function(err){
   else{
     open('http://localhost:' + port)
   }
-})
+});
+
+app.get('/users', function(req, res){
+  res.json([
+    {"id": 1,"firstName": "bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName": "ziadi","lastName":"bilel","email":"ziadi@gmail.com"},
+    {"id": 3,"firstName": "hamla","lastName":"hamla","email":"hamla@gmail.com"}
+  ]);
+});
